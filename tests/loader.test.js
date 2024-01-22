88eafc5a7b3cac3b3c3384f55ffbe11f66358e37
fisker <lionkay@gmail.com>
2024-01-22 13:07:23 +0800

    Minor tweak

---
+++ tests/loader.test.js
@@ -0,0 +1,79 @@
+import test from 'node:test'
+import * as assert from 'node:assert/strict'
+import * as fs from 'node:fs/promises'
+import * as path from 'node:path'
+import {temporaryDirectory as getTemporaryDirectory} from 'tempy'
+import {execa} from 'execa'
+
+async function run({type}) {
+  const directory = getTemporaryDirectory()
+
+  try {
+    await execa('yarn init -y', {cwd: directory})
+
+    const packageJsonFile = path.join(directory, 'package.json')
+    const packageJson = JSON.parse(await fs.readFile(packageJsonFile))
+    await fs.writeFile(
+      packageJsonFile,
+      JSON.stringify({...packageJson, type}, undefined, 2),
+    )
+    await execa('yarn set version berry', {cwd: directory})
+    await execa('yarn', {cwd: directory})
+
+    const file = path.join(directory, 'foo.mjs')
+    const module = new URL(
+      '../fixtures/loaders/yarn-pnp-test.js',
+      import.meta.url,
+    )
+    await fs.writeFile(
+      file,
+      /* Indent */ `
+        import print from ${JSON.stringify(module.href)}
+        console.log(JSON.stringify(print(), undefined, 2))
+      `,
+    )
+
+    return await execa('yarn node foo.mjs --silent', {
+      cwd: directory,
+      env: {FORCE_COLOR: '0'},
+    })
+  } finally {
+    await fs.rm(directory, {force: true, recursive: true})
+  }
+}
+
+test('Yarn PNP', async () => {
+  {
+    const {stdout, stderr} = await run({type: 'module'})
+    assert.equal(stderr, '')
+    const result = JSON.parse(stdout)
+    assert.equal(result['package.json'].type, 'module')
+    assert.equal(result['process.versions.pnp'], '3')
+    const NODE_OPTIONS = result['process.env.NODE_OPTIONS']
+    assert.ok(
+      NODE_OPTIONS.includes('.pnp.cjs'),
+      `Expected ${NODE_OPTIONS} to contain '.pnp.cjs'`,
+    )
+    assert.ok(
+      NODE_OPTIONS.includes('.pnp.loader.mjs'),
+      `Expected ${NODE_OPTIONS} to contain '.pnp.loader.mjs'`,
+    )
+  }
+  {
+    const {stdout, stderr} = await run({type: 'script'})
+    assert.equal(stderr, '')
+    const result = JSON.parse(stdout)
+    assert.equal(result['package.json'].type, 'script')
+
+    assert.equal(result['process.versions.pnp'], '3')
+    const NODE_OPTIONS = result['process.env.NODE_OPTIONS']
+    assert.ok(
+      NODE_OPTIONS.includes('.pnp.cjs'),
+      `Expected ${NODE_OPTIONS} to contain '.pnp.cjs'`,
+    )
+    assert.ok(
+      !NODE_OPTIONS.includes('.pnp.loader.mjs'),
+      `Expected ${NODE_OPTIONS} to NOT contain '.pnp.loader.mjs'`,
+    )
+  }
+})
