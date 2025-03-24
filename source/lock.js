97377c9eb6a62bf587914ecca5b91ef35a9597f3
fisker <lionkay@gmail.com>
2025-03-25 00:51:19 +0800

    Debug

--- source/lock.js
+++ source/lock.js
@@ -1,5 +1,8 @@
 import AtomicsWaitError from './atomics-wait-error.js'
-import {ATOMICS_WAIT_RESULT_TIMED_OUT} from './constants.js'
+import {
+  ATOMICS_WAIT_RESULT_NOT_EQUAL,
+  ATOMICS_WAIT_RESULT_TIMED_OUT,
+} from './constants.js'
 
 const SIGNAL_INDEX = 0
 
@@ -19,9 +22,11 @@ class Lock {
   lock(timeout = Number.POSITIVE_INFINITY) {
     const {semaphore} = this
 
+    const before = this.#messageCount
     // Already unlocked
     const count = Atomics.load(semaphore, SIGNAL_INDEX)
     if (count > this.#messageCount) {
+      console.log({semaphore, before, count})
       this.#messageCount = count
       return
     }
@@ -37,6 +42,10 @@ class Lock {
 
     if (result === ATOMICS_WAIT_RESULT_TIMED_OUT) {
       throw new AtomicsWaitError(result)
+    }
+
+    if (result === ATOMICS_WAIT_RESULT_NOT_EQUAL) {
+      console.log({semaphore, before})
     }
   }
 }
