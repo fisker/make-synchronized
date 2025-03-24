153db27db8afa454f554b02d3d5e10fd64044614
fisker <lionkay@gmail.com>
2025-03-25 00:55:34 +0800

    1e6

--- tests/reliability.test.js
+++ tests/reliability.test.js
@@ -6,7 +6,7 @@ import loadModuleForTests from '../scripts/load-module-for-tests.js'
 const {makeSynchronized} = await loadModuleForTests()
 const synchronize = (url) => makeSynchronized(new URL(url, import.meta.url))
 
-const times = process.env.CI ? 1e5 : 1e4
+const times = process.env.CI ? 1e6 : 1e4
 test(`reliability(${times} runs)`, async () => {
   const {default: identity} = synchronize(
     '../fixtures/asynchronous-modules/async-identity.js',
