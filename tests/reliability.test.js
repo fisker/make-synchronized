406bede437509ae8e9da5af4c50c2d2a5ed640d0
fisker <lionkay@gmail.com>
2025-03-25 00:02:17 +0800

    Add test

---
+++ tests/reliability.test.js
@@ -0,0 +1,18 @@
+import * as assert from 'node:assert/strict'
+import process from 'node:process'
+import test from 'node:test'
+import loadModuleForTests from '../scripts/load-module-for-tests.js'
+
+const {makeSynchronized} = await loadModuleForTests()
+const synchronize = (url) => makeSynchronized(new URL(url, import.meta.url))
+
+const times = process.env.CI ? 1e6 : 1e4
+test(`reliability(${times} runs)`, async () => {
+  const {default: identity} = synchronize(
+    '../fixtures/asynchronous-modules/async-identity.js',
+  )
+
+  for (let index = 0; index < times; index++) {
+    assert.equal(identity(index), index)
+  }
+})
