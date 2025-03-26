f5ec77b8b2d86123cba1ffafa35bc3fa560f728a
fisker <lionkay@gmail.com>
2025-03-26 16:21:48 +0800

    Minor tweak

--- source/for-inline-functions.js
+++ source/for-inline-functions.js
@@ -2,11 +2,11 @@ import {VALUE_INFORMATION__FUNCTION} from './constants.js'
 import Synchronizer from './synchronizer.js'
 
 function makeInlineFunctionSynchronized(implementation) {
-  let code =
+  const code = `export default ${
     typeof implementation === 'function'
       ? implementation.toString()
       : implementation
-  code = `export default ${code}`
+  }`
 
   const module = `data:text/javascript,;${encodeURIComponent(code)}`
   const synchronizer = Synchronizer.create({module})
