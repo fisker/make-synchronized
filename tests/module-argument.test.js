b7f22e1bab775169a9ae5d75feb59733539a536a
fisker <lionkay@gmail.com>
2025-03-26 14:40:37 +0800

    Test non-supported protocol

--- tests/module-argument.test.js
+++ tests/module-argument.test.js
@@ -27,17 +27,75 @@ test('built-module', () => {
   assert.ok(makeModuleSynchronized('node:path').sep, path.sep)
 })
 
+const invalidModuleTypeError = {
+  name: 'TypeError',
+  message: /^'module' should be/,
+}
+
 test('invalid', () => {
   assert.throws(() => {
     makeModuleSynchronized(/* Invalid module */ true)
-  })
+  }, invalidModuleTypeError)
   assert.throws(() => {
     makeModuleSynchronized(/* Invalid module */ 0)
-  })
+  }, invalidModuleTypeError)
   assert.throws(() => {
     makeModuleSynchronized(/* Missing module */)
-  })
+  }, invalidModuleTypeError)
   assert.throws(() => {
-    makeModuleSynchronized('/non-exits-module.js')
-  })
+    makeModuleSynchronized(/* Invalid module */ null)
+  }, invalidModuleTypeError)
+})
+
+const moduleNotFoundError = {
+  name: 'Error',
+  message: /Cannot find module/,
+  code: 'ERR_MODULE_NOT_FOUND',
+}
+test('non-exists', () => {
+  const absolutePath =
+    path.sep === '\\'
+      ? String.raw`C:\non-exits-module.js`
+      : '/non-exits-module.js'
+  const moduleUrl = url.pathToFileURL(absolutePath)
+
+  assert.throws(
+    () => {
+      makeModuleSynchronized(absolutePath)
+    },
+    moduleNotFoundError,
+    `absolute path '${absolutePath}'`,
+  )
+  assert.throws(() => {
+    makeModuleSynchronized({filename: absolutePath})
+  }, moduleNotFoundError)
+  assert.throws(() => {
+    makeModuleSynchronized(moduleUrl)
+  }, moduleNotFoundError)
+  assert.throws(() => {
+    makeModuleSynchronized({href: moduleUrl.href})
+  }, moduleNotFoundError)
+  assert.throws(() => {
+    makeModuleSynchronized({url: moduleUrl.href})
+  }, moduleNotFoundError)
+})
+
+test('relative path', () => {
+  assert.throws(() => {
+    makeModuleSynchronized('./non-exits-module.js')
+  }, invalidModuleTypeError)
+})
+
+const invalidProtocolError = {
+  name: 'Error',
+  message: /Received protocol/,
+  code: 'ERR_UNSUPPORTED_ESM_URL_SCHEME',
+}
+test('non-supported url', () => {
+  assert.throws(() => {
+    makeModuleSynchronized('https://example.com')
+  }, invalidProtocolError)
+  assert.throws(() => {
+    makeModuleSynchronized(URL.createObjectURL(new Blob([''])))
+  }, invalidProtocolError)
 })
