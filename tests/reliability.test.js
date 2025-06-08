d7faa8a08a398fddf9728a9d731573f68e838ec8
fisker <lionkay@gmail.com>
2025-06-08 21:34:02 +0800

    Rename variable

--- tests/reliability.test.js
+++ tests/reliability.test.js
@@ -6,17 +6,17 @@ import loadModuleForTests from '../scripts/load-module-for-tests.js'
 const {makeSynchronized} = await loadModuleForTests()
 const synchronize = (url) => makeSynchronized(new URL(url, import.meta.url))
 
-const times = process.env.CI ? 1e6 : 1e4
-test(`reliability(${times} runs)`, async () => {
+const iterations = process.env.CI ? 1e6 : 1e4
+test(`reliability(${iterations} iterations)`, async () => {
   const {default: identity} = synchronize(
     '../fixtures/asynchronous-modules/async-identity.js',
   )
 
-  for (let index = 0; index < times; index++) {
+  for (let index = 0; index < iterations; index++) {
     try {
       assert.equal(identity(index), index)
     } catch (error) {
-      console.log(`Failed on ${index + 1}/${times} run.`)
+      console.log(`Failed on ${index + 1}/${iterations} run.`)
       throw error
     }
   }
